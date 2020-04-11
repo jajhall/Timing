b0794105a9e38813f946e55b5a49d1a3d794ee94
Julian Hall <jajhall@ed.ac.uk>
2020-04-11 15:03:36 +0100

    Add files via upload

---
+++ Timer.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "HighsTimer.h"
+#include "ClockProperties.h"
+
+double sumLog(const int n) {
+  double sum;
+  for(int i = 0; i < n; i++)
+    sum += std::log(i+1);
+  return sum;
+}
+
+void multipleTimeCalls(HighsTimer& timer, const int n) {
+  for(int i = 0; i < n; i++)
+    timer.getWallTime();
+}
+
+void multipleTimeCalls0(HighsTimer& timer, const int n) {
+  for(int i = 0; i < n; i++)
+    timer.getWallTime0();
+}
+
+void multipleTimeCalls1(HighsTimer& timer, const int n) {
+  for(int i = 0; i < n; i++)
+    timer.getWallTime1();
+}
+
+void multipleTimeCalls2(HighsTimer& timer, const int n) {
+  for(int i = 0; i < n; i++)
+    timer.getWallTime2();
+}
+
+void multipleTickCalls(HighsTimer& timer, const int n) {
+  for(int i = 0; i < n; i++)
+    timer.getWallTick();
+}
+
+int main () {
+  int num;
+  report();
+  std::cout << "Enter number of calls: ";
+  std::cin >> num;
+  if (!num) return 1;
+  HighsTimer timer;
+  int work_clock = timer.clock_def("Work", "Wrk");
+  int time_clock = timer.clock_def("Time", "Tim");
+  int time0_clock = timer.clock_def("Time0", "Tt0");
+  int time1_clock = timer.clock_def("Time1", "Tt1");
+  int time2_clock = timer.clock_def("Time2", "Tt2");
+  int tick_clock = timer.clock_def("Tock", "Tik");
+  std::vector<int> clock_list;
+  clock_list.push_back(work_clock);
+  clock_list.push_back(time_clock);
+  clock_list.push_back(time0_clock);
+  clock_list.push_back(time1_clock);
+  clock_list.push_back(time2_clock);
+  clock_list.push_back(tick_clock);
+  timer.startRunHighsClock();
+
+  timer.start(work_clock);
+  double sum_log = sumLog(num);
+  timer.stop(work_clock);
+
+  timer.start(time_clock);
+  multipleTimeCalls(timer, num);
+  timer.stop(time_clock);
+
+  timer.start(time0_clock);
+  multipleTimeCalls0(timer, num);
+  timer.stop(time0_clock);
+
+  timer.start(time1_clock);
+  multipleTimeCalls1(timer, num);
+  timer.stop(time1_clock);
+
+  timer.start(time2_clock);
+  multipleTimeCalls2(timer, num);
+  timer.stop(time2_clock);
+
+  timer.start(tick_clock);
+  multipleTickCalls(timer, num);
+  timer.stop(tick_clock);
+
+  timer.stopRunHighsClock();
+
+  // Totally abuse the clock call mechanism to aid reporting
+  timer.clock_num_call[time_clock] = num;
+  timer.clock_num_call[time0_clock] = num;
+  timer.clock_num_call[time1_clock] = num;
+  timer.clock_num_call[time2_clock] = num;
+  timer.clock_num_call[tick_clock] = num;
+  timer.report("", clock_list);
+  return 0;
+}
